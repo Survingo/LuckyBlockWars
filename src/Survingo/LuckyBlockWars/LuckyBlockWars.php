c55997f0b65f7830b8500edc1ff0dd0f0322b5e4
Survingo <thehydra.tk@gmail.com>
2016-06-29 22:08:30 +0200

    first code

--- src/Survingo/LuckyBlockWars/LuckyBlockWars.php
+++ src/Survingo/LuckyBlockWars/LuckyBlockWars.php
@@ -15,9 +15,42 @@ limitations under the License.
 
 namespace Survingo\LuckyBlockWars;
 
+use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
+use pocketmine\math\Vector3;
 use pocketmine\plugin\PluginBase;
 
 class LuckyBlockWars extends PluginBase implements Listener{
-  //
+   const UNLUCKY_STUFF = array(
+      $boom = new \pocketmine\level\Explosion($event->getBlock(), mt_rand($this->getConfig()->get("explosion_min"), $this->getConfig()->get("explosion_max")));
+      $boom->explodeA();
+      );
+   const NORMAL_STUFF = array(
+      
+      );
+   const LUCKY_STUFF = (
+      
+      );
+   
+  public function onBlockBreak(BlockBreakEvent $event){
+     if($event->getBlock()->getId() == $this->getConfig()->get("luckyblock-id")){
+        if($event->getPlayer()->hasPermission("lbw.use")){
+           switch (mt_rand(1,3)){
+              case 1: LuckyBlockWars::getRandom(LuckyBlockWars::UNLUCKY_STUFF);
+              break;
+              case 2: LuckyBlockWars::getRandom(LuckyBlockWars::NORMAL_STUFF);
+              break;
+              case 3: LuckyBlockWars::getRandom(LuckyBlockWars::LUCKY_STUFF);
+              break;
+           }
+        }else{
+           $event->getPlayer()->sendMessage($this->getConfig()->get("not_allowed"));
+        }
+     }
+  }
+ 
+ public static function getRandom(array $things){
+    if(is_array($things)) return $things[array_rand($things, 1)];
+ }
+ 
 }
