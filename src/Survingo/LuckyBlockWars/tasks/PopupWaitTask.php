b24242ce43e011877a3148b804a435964bf25b0b
Survingo <thehydra.tk@gmail.com>
2016-06-30 20:21:46 +0200

    Update PopupWaitTask.php

--- src/Survingo/LuckyBlockWars/tasks/PopupWaitTask.php
+++ src/Survingo/LuckyBlockWars/tasks/PopupWaitTask.php
@@ -1,3 +1,23 @@
 <?php
 
-namespace Survingo\
+namespace Survingo\LuckyBlockWars\tasks;
+
+use pocketmine\scheduler\PluginTask;
+use Survingo\LuckyBlockWars\LuckyBlockWars;
+
+class PopupWaitTask extends PluginTask{
+  
+  private $plugin;
+  
+  public function __construct(LuckyBlockWars $plugin){
+    parent::__construct($plugin);
+    $this->plugin $plugin;
+  }
+  
+  public function onRun($currentTick){
+    foreach($this->plugin->getPlayersInGame() as $player){
+      $this->plugin->getServer()->getPlayer($player)->sendPopup("§6Waiting for players...");
+    }
+  }
+  
+}
