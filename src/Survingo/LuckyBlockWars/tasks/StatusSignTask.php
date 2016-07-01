e85f80d73431e2f8a4d06febc2ae6ccb23e128a1
Survingo <thehydra.tk@gmail.com>
2016-07-01 20:35:49 +0200

    Update StatusSignTask.php

--- src/Survingo/LuckyBlockWars/tasks/StatusSignTask.php
+++ src/Survingo/LuckyBlockWars/tasks/StatusSignTask.php
@@ -17,14 +17,16 @@ class StatusSignTask extends PluginTask{
   }
   
   public function onRun($currentTick){
-    $tile = $this->plugin->getServer()->getLevelByName($this->plugin->cfg["sign-world"])->getTile(new Vector3($this->plugin->cfg["sign-x"], $this->plugin->cfg["sign-y"], $this->plugin->cfg["sign-z"]));
-    if($tile instanceof Sign){
-      if($this->plugin->running == true){
-        $tile->setText("§l[§6L§eB§cW§f]", "§cRunning", "Players", "§1" . count($this->plugin->players) . "§7/§9" . $this->plugin->cfg["needed-players"]);
-      }elseif($this->plugin->running == false and count($this->plugin->reds) !== $this->plugin->cfg["needed-players"]){
-        $tile->setText("§l[§6L§eB§cW§f]", "§aJoin", "Players", "§1" . count($this->plugin->players) . "§7/§9" . $this->plugin->cfg["needed-players"]);
-      }elseif($this->plugin->running == false and count($this->plugin->players) == $this->plugin->cfg["needed-players"]){
-        $tile->setText("§l[§6L§eB§cW§f]", "§6Full", "Players", "§1" . count($this->plugin->players) . "§7/§9" . $this->plugin->cfg["needed-players"]);
+    if($this->plugin->cfg["sign-mode"] === true){
+      $tile = $this->plugin->getServer()->getLevelByName($this->plugin->cfg["sign-world"])->getTile(new Vector3($this->plugin->cfg["sign-x"], $this->plugin->cfg["sign-y"], $this->plugin->cfg["sign-z"]));
+      if($tile instanceof Sign){
+        if($this->plugin->running == true){
+          $tile->setText("§l[§6L§eB§cW§f]", "§cRunning", "Players", "§1" . count($this->plugin->players) . "§7/§9" . $this->plugin->cfg["needed-players"]);
+          }elseif($this->plugin->running == false and count($this->plugin->reds) !== $this->plugin->cfg["needed-players"]){
+            $tile->setText("§l[§6L§eB§cW§f]", "§aJoin", "Players", "§1" . count($this->plugin->players) . "§7/§9" . $this->plugin->cfg["needed-players"]);
+            }elseif($this->plugin->running == false and count($this->plugin->players) == $this->plugin->cfg["needed-players"]){
+              $tile->setText("§l[§6L§eB§cW§f]", "§6Full", "Players", "§1" . count($this->plugin->players) . "§7/§9" . $this->plugin->cfg["needed-players"]);
+            }
       }
     }
   }
