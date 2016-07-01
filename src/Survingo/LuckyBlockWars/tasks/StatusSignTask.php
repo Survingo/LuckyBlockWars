7ed18e20b50d42f176afee47fe6a60ec92913c34
Survingo <thehydra.tk@gmail.com>
2016-07-01 19:35:11 +0200

    Update StatusSignTask.php

--- src/Survingo/LuckyBlockWars/tasks/StatusSignTask.php
+++ src/Survingo/LuckyBlockWars/tasks/StatusSignTask.php
@@ -1,1 +1,32 @@
 <?php
+
+namespace Survingo\LuckyBlockWars\tasks;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\math\Vector3;
+use pocketmine\tile\Sign;
+use Survingo\LuckyBlockWars\LuckyBlockWars;
+
+class StatusSignTask extends PluginTask{
+  
+  private $plugin;
+  
+  public function __construct(LuckyBlockWars $plugin){
+    parent::__construct($plugin);
+    $this->plugin = $plugin;
+  }
+  
+  public function onRun($currentTick){
+    $tile = $this->plugin->getServer()->getLevelByName($this->plugin->cfg["sign-world"])->getTile(new Vector3($this->plugin->cfg["sign-x"], $this->plugin->cfg["sign-y"], $this->plugin->cfg["sign-z"]));
+    if($tile instanceof Sign){
+      if($this->plugin->running == true){
+        $tile->setText("§l[§6L§eB§cW§f]", "§cRunning", "Players", "§1" . count($this->plugin->players) . "§7/§9" . $this->plugin->cfg["needed-players"]);
+      }elseif($this->plugin->running == false and count($this->plugin->reds) !== $this->plugin->cfg["needed-players"]){
+        $tile->setText("§l[§6L§eB§cW§f]", "§aJoin", "Players", "§1" . count($this->plugin->players) . "§7/§9" . $this->plugin->cfg["needed-players"]);
+      }elseif($this->plugin->running == false and count($this->plugin->players) == $this->plugin->cfg["needed-players"]){
+        $tile->setText("§l[§6L§eB§cW§f]", "§6Full", "Players", "§1" . count($this->plugin->players) . "§7/§9" . $this->plugin->cfg["needed-players"]);
+      }
+    }
+  }
+  
+}
