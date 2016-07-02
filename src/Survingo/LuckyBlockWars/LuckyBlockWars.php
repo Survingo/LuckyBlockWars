8f3b242cafe8f762200d0609655dc11cf75596ee
Survingo <thehydra.tk@gmail.com>
2016-07-02 18:09:05 +0200

    Update LuckyBlockWars.php

--- src/Survingo/LuckyBlockWars/LuckyBlockWars.php
+++ src/Survingo/LuckyBlockWars/LuckyBlockWars.php
@@ -51,28 +51,6 @@ class LuckyBlockWars extends PluginBase implements Listener{
   public function onDisable(){
      $this->getServer()->getLogger()->info($this->prefix . "Disabling plugin...");
   }
-  
-  public function onBlockBreak(BlockBreakEvent $event){
-     if($event->getBlock()->getId() == $this->cfg["luckyblock-id"]){
-        if($this->running === true){
-           if($event->getPlayer()->hasPermission("lbw.game.use")){
-              switch (mt_rand(1,3)){
-                 case 1: $this->getRandom($this->unluckyBlockStuff($event->getBlock()));
-                 break;
-                 case 2: $this->getRandom($this->normalBlockStuff($event->getBlock()));
-                 break;
-                 case 3: $this->getRandom($this->luckyBlockStuff($event->getBlock()));
-                 break;
-              }
-           }else{
-              $event->setCancelled(true);
-              $event->getPlayer()->sendMessage($this->cfg["not-allowed-to-use-luckyblock"]);
-           }
-        }else{
-           $event->getPlayer()->sendMessage($this->cfg["game-is-not-running"]);
-        }
-     }
-  }
  
  public function getRandom(array $things){
     if(is_array($things)) return $things[array_rand($things, 1)];
@@ -93,55 +71,6 @@ class LuckyBlockWars extends PluginBase implements Listener{
     return array(
        //$test->test();
  );}
- 
- public function onInteract(PlayerInteractEvent $event){
-    if($event->getBlock()->getX() === $this->cfg["sign-x"] and $event->getBlock()->getY() === $this->cfg["sign-y"] and $event->getBlock()->getZ() === $this->cfg["sign-z"]){
-       if($this->running == false){
-          $this->addToGame($event->getPlayer()->getName());
-       }else{
-          $event->getPlayer()->sendMessage($this->cfg["game-is-running"]);
-       }
-    }
- }
- 
- public function onDeath(PlayerDeathEvent $event){
-    if($this->running == true){
-       if(in_array($event->getEntity()->getName(), $this->players)){
-          unset($this->players{array_search($event->getEntity()->getName(), $this->players)});
-          $event->setDeathMessage($this->cfg["death-message"]);
-          $event->getEntity()->teleport($this->getServer()->getLevelByName($this->cfg["respawn-level"])->getSafeSpawn());
-       }
-       if(count($this->players == 1)){
-          $this->getServer()->getPlayer($this->players)->teleport($this->getServer()->getLevelByName($this->cfg["respawn-level"])->getSafeSpawn());
-          $this->getServer()->broadcastMessage(str_replace(["{name}", "health"], [$this->players, $this->getServer()->getPlayer($this->players)->getHealth()], $this->cfg["won-broadcast"]));
-          $this->getServer()->getPlayer(this->players)->setHealth(20);
-          unset($this->players{array_search($this->players, $this->players)});
-          $this->running = false;
-       }
-    }
- }
- 
- public function onSignChange(SignChangeEvent $event){
-    if($event->getLine(0) == "[LBW]" or $event->getLine(0) == "[LuckyBlock]" or $event->getLine(0) == "/lbw join"){
-       if($event->getPlayer()->hasPermission("lbw.game.create-signs")){
-          $this->getConfig()->set("sign-x", $event->getBlock()->getX());
-          $this->getConfig()->save();
-          $this->getConfig()->set("sign-y", $event->getBlock()->getY());
-          $this->getConfig()->save();
-          $this->getConfig()->set("sign-z", $event->getBlock()->getZ());
-          $this->getConfig()->save();
-          $this->getConfig()->set("sign-world", $event->getPlayer()->getLevel()->getName());
-          $this->getConfig()->save();
-          $this->getConfig()->set("sign-mode", true);
-          $this->getConfig()->save;
-          $event->setLine(0, "§l[§6L§eB§cW§f]");
-          $event->setLine(1, "§aJoin");
-       }else{
-          $event->setLine(0, "No");
-          $event->setLine(1, "Permission");
-       }
-    }
- }
 
  public function addToGame($name){
     if(count($this->players !== $this->cfg["needed-players"])){
