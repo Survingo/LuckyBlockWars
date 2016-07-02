cdcfc86a98c931fca9a87d7273f7b1e5b3efe13c
Survingo <thehydra.tk@gmail.com>
2016-07-02 17:54:57 +0200

    Update EventManager.php

--- src/Survingo/LuckyBlockWars/EventManager.php
+++ src/Survingo/LuckyBlockWars/EventManager.php
@@ -5,7 +5,7 @@ namespace Survingo\LuckyBlockWars;
 use pocketmine\event\Listener;
 
 class EventManager implements Listener{
-  
+  /** @var LuckyBlockWars */
   private $plugin;
   
   public function __construct(LuckyBlockWars $plugin){
