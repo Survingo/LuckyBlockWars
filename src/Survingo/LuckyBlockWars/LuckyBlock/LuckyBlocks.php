e183be7ad5b73152048292050823b5fb633a171d
Survingo <thehydra.tk@gmail.com>
2016-07-05 21:31:48 +0200

    Update LuckyBlocks.php

--- src/Survingo/LuckyBlockWars/LuckyBlock/LuckyBlocks.php
+++ src/Survingo/LuckyBlockWars/LuckyBlock/LuckyBlocks.php
@@ -13,18 +13,20 @@ class LuckyBlocks{
   
   private $block;
   
+  private $player;
+  
   public function __construct(LuckyBlockWars $plugin, $block, Player $player){
-    $this->block = $block;
     $this->plugin = $plugin;
     $this->block = $block;
+    $this->player = $player;
   }
   
   public function run(){
     switch(mt_rand(1,2)){
-      case 1: $player->getLevel()->dropItem($this->block, Item::get(Item::DIAMOND,0,1,"{display:{Name:"§b§lLucky Diamond"}}"));
+      case 1: $this->player->getLevel()->dropItem($this->block, Item::get(Item::DIAMOND,0,1,"{display:{Name:"§b§lLucky Diamond"}}"));
       break;
-      case 2: $player->getLevel()->dropItem($this->block, Item::get(Item::BOW,0,1,"{display:{Name:"§6§lLucky Bow"},ench:[{id:22s,lvl:1s},{id:20s,lvl:2s}]}"));
-      $player->getLevel()->dropItem($this->block, Item::get(Item::ARROW));
+      case 2: $this->player->getLevel()->dropItem($this->block, Item::get(Item::BOW,0,1,"{display:{Name:"§6§lLucky Bow"},ench:[{id:22s,lvl:1s},{id:20s,lvl:2s}]}"));
+      $this->player->getLevel()->dropItem($this->block, Item::get(Item::ARROW));
       break;
     }
   }
