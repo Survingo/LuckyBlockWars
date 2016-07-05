df0dd7735d59304ccf569742bea0c44b20ec642c
Survingo <thehydra.tk@gmail.com>
2016-07-05 19:36:31 +0200

    Update LuckyBlocks.php

--- src/Survingo/LuckyBlockWars/LuckyBlock/LuckyBlocks.php
+++ src/Survingo/LuckyBlockWars/LuckyBlock/LuckyBlocks.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace Survingo\LuckyBlocks\LuckyBlock;
+
+class LuckyBlocks{
+  
+  public function __construct($plugin, $block){
+    $this->plugin = $plugin;
+    $this->block = $block;
+  }
+  
+  public function doLuckyStuff(){
+    //
+  }
+  
+}
