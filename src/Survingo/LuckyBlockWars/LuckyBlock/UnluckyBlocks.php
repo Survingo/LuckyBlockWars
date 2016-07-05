b7b094cdfc08a269dd874ef358d69c1538df99f7
Survingo <thehydra.tk@gmail.com>
2016-07-05 20:28:09 +0200

    Update UnluckyBlocks.php

--- src/Survingo/LuckyBlockWars/LuckyBlock/UnluckyBlocks.php
+++ src/Survingo/LuckyBlockWars/LuckyBlock/UnluckyBlocks.php
@@ -1,1 +1,30 @@
 <?php
+
+namespace Survingo\LuckyBlocks\LuckyBlock;
+
+use pocketmine\Player;
+
+use Survingo\LuckyBlockWars\LuckyBlockWars;
+
+class UnluckyBlocks{
+  
+  private $plugin;
+  
+  private $block;
+  
+  public function __construct(LuckyBlockWars $plugin, $block, Player $player){
+    $this->block = $block;
+    $this->plugin = $plugin;
+    $this->block = $block;
+  }
+  
+  public function run(){
+    switch(mt_rand(1,2)){
+      case 1: (new \pocketmine\level\Explosion($this->block, mt_rand($this->plugin->getConfig()->get("min-explosion"), $this->plugin->getConfig("max-explosion"))))->explodeA();
+      break;
+      case 2:
+      break;
+    }
+  }
+  
+}
