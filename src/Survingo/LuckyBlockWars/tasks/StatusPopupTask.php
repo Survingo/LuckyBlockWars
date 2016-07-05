04aabd7b226f75ed49e08aee36e4156cd0ad4356
Survingo <thehydra.tk@gmail.com>
2016-07-05 19:08:48 +0200

    Create StatusPopupTask.php

---
+++ src/Survingo/LuckyBlockWars/tasks/StatusPopupTask.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+   Copyright 2016 Survingo
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+namespace Survingo\LuckyBlockWars\tasks;
+
+use pocketmine\scheduler\PluginTask;
+use Survingo\LuckyBlockWars\LuckyBlockWars;
+
+class StatusPopupTask extends PluginTask{
+  /** @var LuckyBlockWars */
+  private $plugin;
+  
+  public function __construct(LuckyBlockWars $plugin){
+    parent::__construct($plugin);
+    $this->plugin = $plugin;
+  }
+  
+  public function onRun($currentTick){
+    foreach($this->plugin->getServer()->getPlayer($this->plugin->players) as $player){
+      $player->sendPopup(str_replace("{players}", $this->plugin->players, $this->plugin->msg["status-popup"]));
+    }
+  }
+  
+}
