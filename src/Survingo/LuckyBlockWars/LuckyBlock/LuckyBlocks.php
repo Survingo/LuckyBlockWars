89172dab3c2e0388cc6d26dd6dc855ae2c1c0a00
Survingo <thehydra.tk@gmail.com>
2016-07-05 20:26:47 +0200

    Update LuckyBlocks.php

--- src/Survingo/LuckyBlockWars/LuckyBlock/LuckyBlocks.php
+++ src/Survingo/LuckyBlockWars/LuckyBlock/LuckyBlocks.php
@@ -2,18 +2,28 @@
 
 namespace Survingo\LuckyBlocks\LuckyBlock;
 
+use pocketmine\Player;
 use Survingo\LuckyBlockWars\LuckyBlockWars;
 
 class LuckyBlocks{
   
+  private $plugin;
+  
   private $block;
   
-  public function __construct(LuckyBlockWars $plugin, $block){
+  public function __construct(LuckyBlockWars $plugin, $block, Player $player){
+    $this->block = $block;
+    $this->plugin = $plugin;
     $this->block = $block;
   }
   
-  public function doLuckyStuff(){
-    //
+  public function run(){
+    switch(mt_rand(1,2)){
+      case 1:
+      break;
+      case 2:
+      break;
+    }
   }
   
 }
