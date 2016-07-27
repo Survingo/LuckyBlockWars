11d9d85149c9d248f14cc6bc0f2040f74e970e76
Survingo <thehydra.tk@gmail.com>
2016-07-27 19:26:54 +0200

    How could that happen?

--- src/Survingo/LuckyBlockWars/game/UnluckyBlocks.php
+++ src/Survingo/LuckyBlockWars/game/UnluckyBlocks.php
@@ -1,25 +1,26 @@
 <?php
 
-/*
- _               _          ____  _            _   __        __             
-| |   _   _  ___| | ___   _| __ )| | ___   ___| | _\ \      / /_ _ _ __ ___ 
-| |  | | | |/ __| |/ / | | |  _ \| |/ _ \ / __| |/ /\ \ /\ / / _` | '__/ __|
-| |__| |_| | (__|   <| |_| | |_) | | (_) | (__|   <  \ V  V / (_| | |  \__ \
-|_____\__,_|\___|_|\_\\__, |____/|_|\___/ \___|_|\_\  \_/\_/ \__,_|_|  |___/
-                      |___/
-   Copyright 2016 Survingo
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-limitations under the License.
+/**
+ * 
+ *  _               _          ____  _            _   __        __             
+ * | |   _   _  ___| | ___   _| __ )| | ___   ___| | _\ \      / /_ _ _ __ ___ 
+ * | |  | | | |/ __| |/ / | | |  _ \| |/ _ \ / __| |/ /\ \ /\ / / _` | '__/ __|
+ * | |__| |_| | (__|   <| |_| | |_) | | (_) | (__|   <  \ V  V / (_| | |  \__ \
+ * |_____\__,_|\___|_|\_\\__, |____/|_|\___/ \___|_|\_\  \_/\_/ \__,_|_|  |___/
+ *                       |___/
+ *   Copyright 2016 Survingo
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
 */
 
-namespace Survingo\LuckyBlocks\game;
+namespace Survingo\LuckyBlockWars\game;
 
 use pocketmine\block\Block;
 use pocketmine\Player;
@@ -27,11 +28,11 @@ use pocketmine\item\Item;
 use Survingo\LuckyBlockWars\LuckyBlockWars;
 
 class UnluckyBlocks{
-  
+  /** @var Main $plugin */
   private $plugin;
-  
+  /** @var Block $block */
   private $block;
-  
+  /** @var Player $player */
   private $player;
   
   public function __construct(LuckyBlockWars $plugin, Block $block, Player $player){
@@ -44,7 +45,7 @@ class UnluckyBlocks{
     switch(mt_rand(1,2)){
       case 1: (new \pocketmine\level\Explosion($this->block, mt_rand($this->plugin->getConfig()->get("min-explosion"), $this->plugin->getConfig("max-explosion"))))->explodeA();
       break;
-      case 2:
+      case 2: //do anything else
       break;
     }
   }
